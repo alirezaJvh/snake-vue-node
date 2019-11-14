b7cea24b2a5682dd66d5fb4bb64ad315515e74dd
alireza <alireza.jvh98@gmail.com>
2019-11-14 12:05:08 +0330

    draw snake on board

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -9,11 +9,12 @@
             <v-col cols="12"
                    class="d-flex justify-center align-center my-2 board-container">
                 <div ref="board" class="d-flex flex-row flex-wrap ">
-                    <div v-for="x in boardWidth"
-                         :key="x">
-                        <div v-for="y in boardHeight"
-                             :key="y">
+                    <div v-for="y in boardHeight"
+                         :key="y">
+                        <div v-for="x in boardWidth"
+                             :key="x">
                             <div :ref="`${x}-${y}`"
+                                 :id="`${x}-${y}`"
                                  class="cell d-flex justify-center align-center "
                                  :style="{width:`${cellWidth}px`, height: `${cellHeight}px`}">
                                 <div class="food"
@@ -29,115 +30,70 @@
 </template>
 
 <script>
+    import boardMixin from '../mixins/board'
+
     export default {
         name: 'playground',
         components: {},
         data: () => ({
-            boardWidth: 16, // means 50 cell not 50px
-            boardHeight: 16,
-            cellWidth: 40, // for example means each cell width is 2px
-            cellHeight: 40,
-            cells: {}
+            snake: {
+                initLength: 4,
+                direction: 'right',
+                body: []
+            },
+            scores: 0
         }),
+        mixins: [boardMixin],
         methods: {
-            makeBoardGame() {
-                this.initBoard();
-                this.initCell();
-                this.foodScoreStyle()
+
+            initSnake() {
+                let [xHead, yHead] = [8, 10];
+                let body;
+                for (let i = 0; i < this.snake.initLength; i++) {
+                    body = this.$refs[`${xHead}-${yHead - i}`][0];
+                    this.snake.body.push(body)
+                }
+                this.$log.debug(this.snake);
+                this.drawSnake()
             },
-            initBoard() {
-                this.$log.debug('board', this.$refs);
-                let board = this.$refs.board;
-                board.style.width = `${this.boardWidth * this.cellWidth}px`;
-                board.style.height = `${this.boardHeight * this.cellHeight}px`;
-            },
-            initCell() {
-                this.$log.debug('init cell');
-                let x = 1;
-                while (x <= this.boardWidth) {
-                    let y = 1;
-                    while (y <= this.boardHeight) {
-                        this.cells[`${x}-${y}`] = {
-                            x,
-                            y,
-                            food: this.initFoodScore(),
-                            snake: false
-                        };
-                        y += 1;
+            drawSnake() {
+                for (let i = 0; i < this.snake.body.length; i++) {
+                    if (i > 0) {
+                        this.getSnakeBodyPart(i).appendChild(this.addSnakeBody('snake-body'))
+                    } else {
+                        this.getSnakeBodyPart(i).appendChild(this.addSnakeBody('snake-head'))
                     }
-                    x += 1;
                 }
-                this.$log.debug('foods', this.cells)
-            },
-            initFoodScore() {
-                let score = Math.ceil((Math.random() * 10));
-                return (score === 10 ? 9 : score);
-            },
-            randomBackground() {
-                let bgColor = Math.floor(Math.random() * 10);
-                switch (bgColor) {
-                    case 0:
-                        return 'success lighten-1';
-                    case 1:
-                        return 'warning lighten-1';
-                    case 2:
-                        return 'warning lighten-3';
-                    case 3:
-                        return 'success lighten-3';
-                    case 4:
-                        return 'error lighten-1';
-                    case 5:
-                        return 'error lighten-3';
-                    case 6:
-                        return 'info lighten-3';
-                    case 7:
-                        return 'purple lighten-3';
-                    case 8:
-                        return 'grey darken-1';
-                    case 9:
-                        return 'grey lighten-5';
+
+                if (this.canSnakeEatFooad()) {
+                    this.increaseSnakeScore();
+                    // this.addSnakeBodyPart();
                 }
+
             },
-            foodScoreStyle() {
-                const coef = 3;
-                let x = 1;
-                while (x <= this.boardWidth) {
-                    let y = 1;
-                    while (y <= this.boardHeight) {
-                        let cell = this.$refs[`${x}-${y}`][0];
-                        let food = cell.children[0];
-                        food.style.width = `${this.cells[`${x}-${y}`].food * coef}px`;
-                        food.style.height = `${this.cells[`${x}-${y}`].food * coef}px`;
-                        this.addAnimation(food);
-                        y++;
-                    }
-                    x++;
-                }
+            increaseSnakeScore() {
+                const c = 5;
+                let score = this.snake.body[0].food;
+                this.$log.debug(this.snake.body[0]);
+                this.scores = c * score + 1
+
             },
-          addAnimation(food) {
-              const time = '3.5s';
-              switch (Math.ceil(Math.random() * 5)) {
-                case 1:
-                  food.style.animation = `zoomInRight ${time}`;
-                      break;
-                case 2:
-                  food.style.animation = `zoomInLeft ${time}`;
-                  break;
-                case 3:
-                  food.style.animation = `zoomInUp ${time}`;
-                  break;
-                case 4:
-                  food.style.animation = `zoomInDown ${time}`;
-                  break;
-                case 5:
-                  food.style.animation = `zoomInUp ${time}`;
-                  break;
-              }
-              this.$log.debug()
-          }
+            canSnakeEatFooad() {
+                // snake can meat food when its body length is one
+                return this.snake.body.length === 1
+            },
+            getSnakeBodyPart(index) {
+                return this.snake.body[index]
+            },
+            addSnakeBody(snakeClass) {
+                let snakeBody = document.createElement('div');
+                snakeBody.classList.add(snakeClass);
+                return snakeBody
+            }
         },
         mounted() {
-            this.makeBoardGame()
+            this.makeBoardGame();
+            this.initSnake();
         }
     }
 </script>
@@ -159,12 +115,32 @@
         border: 1px solid rgba(159, 159, 159, 0.75) !important;
         animation: zoomIn 1.5s;
         background: #51b0ff;
+        position: relative;
     }
 
     .food {
         border-radius: 50%;
         box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
     }
+
+    .snake-body, .snake-head {
+        height: 100%;
+        width: 100%;
+        z-index: 100000;
+        position: absolute;
+        animation: zoomInUp 3s;
+    }
+
+    .snake-body {
+        background: #FFEA00;
+    }
+
+    .snake-head {
+        background: #FF6F00;
+    }
+
+    /***************************/
+    /* animation */
 
     .fadeIn {
         animation: fadeIn 2.5s;
